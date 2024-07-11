d103ca55b821d41575430db144898451d16c8c13
josh-alhassan <saintjosh26@gmail.com>
2024-07-11 12:43:01 +0100

    exercise complete

--- src/App.js
+++ src/App.js
@@ -25,9 +25,29 @@ export default function App() {
 }
 
 function Accordion({ data }) {
-  return <div className="accordion">TODO</div>;
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
+    </div>
+  );
 }
 
 function AccordionItem({ num, title, text }) {
   const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && <div className="content-box">{text} </div>}
+    </div>
+  );
 }
